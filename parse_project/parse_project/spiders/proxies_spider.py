abf03c77e9d924cdf92a5f2038a6b08b03a33810
Sergey Byatov <biatov.s@gmail.com>
2017-04-14 22:13:36 +0300

    Get inline elements from <style>...</style>

---
+++ parse_project/parse_project/spiders/proxies_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class ProxiesSpider(scrapy.Spider):
+    name = "proxies"
+    start_urls = [
+        'http://proxylist.hidemyass.com/',
+    ]
+
+    def parse(self, response):
+        # page = response.url.split("/")[-2]
+        # filename = 'proxies-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        for proxy in response.selector.css('tbody tr'):
+            item = dict()
+
+            item['dirty_elements'] = proxy.css('td')[1].css('span style::text').extract()[0].split()
+            item['inline_elements'] = list(filter(lambda s: 'none' not in s, item['dirty_elements']))
+            item['clear_elements'] = list(map(lambda s: s.split('{')[0][1:], item['inline_elements']))
+
+            yield item
