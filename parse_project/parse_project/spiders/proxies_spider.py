36a0a7d4992342560a9bdddcf411af52b990cbe0
Sergey Byatov <biatov.s@gmail.com>
2017-04-14 22:32:23 +0300

    find port

--- parse_project/parse_project/spiders/proxies_spider.py
+++ parse_project/parse_project/spiders/proxies_spider.py
@@ -8,12 +8,16 @@ class ProxiesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+
         # page = response.url.split("/")[-2]
         # filename = 'proxies-%s.html' % page
         # with open(filename, 'wb') as f:
         #     f.write(response.body)
+
         for proxy in response.selector.css('tbody tr'):
             item = dict()
+
+            item['port'] = proxy.css('td')[2].xpath('text()').extract()[0].strip()
 
             item['dirty_elements'] = proxy.css('td')[1].css('span style::text').extract()[0].split()
             item['inline_elements'] = list(filter(lambda s: 'none' not in s, item['dirty_elements']))
