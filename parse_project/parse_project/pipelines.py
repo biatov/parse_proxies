4018dc1d9004dc175432727b466be76e5a465d65
Sergey Byatov <biatov.s@gmail.com>
2017-04-17 21:41:04 +0300

    some comments for search.py

--- parse_project/parse_project/pipelines.py
+++ parse_project/parse_project/pipelines.py
@@ -5,7 +5,28 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+import os
+
 
 class ParseProjectPipeline(object):
+    """
+    Create table & Record scraped data
+    """
     def process_item(self, item, spider):
-        return item
+        database = 'parsed_data.sqlite'
+        db = create_engine('sqlite:///%s' % database, echo=False)
+        metadata = MetaData(db)
+
+        if not os.path.exists(database):
+            table = Table('proxies', metadata,
+                          Column('id', Integer, primary_key=True),
+                          Column('ip_address', String(15)),
+                          Column('port', String(4)),
+                          )
+            table.create()
+        else:
+            table = Table('proxies', metadata, autoload=True)
+
+        i = table.insert()
+        i.execute(ip_address=item['ip_address'], port=item['port'])
--- parse_project/parse_project/spiders/proxies_spider.py
+++ parse_project/parse_project/spiders/proxies_spider.py
@@ -1,6 +1,8 @@
 import scrapy
 
 from parse_project.search import get_ip, without_style_garb
+from parse_project.items import ParseProjectItem
+from parse_project.pipelines import ParseProjectPipeline
 
 
 class ProxiesSpider(scrapy.Spider):
@@ -13,13 +15,8 @@ class ProxiesSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        page = response.url.split("/")[-2]
-        filename = 'proxies-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-
         for proxy in response.selector.css('table.hma-table tbody tr'):
-            item = dict()
+            item = ParseProjectItem()
 
             dirty_elements = proxy.css('td')[1].css('span style::text').extract()[0].split()
             inline_elements = list(filter(lambda s: 'none' not in s, dirty_elements))
@@ -31,8 +28,11 @@ class ProxiesSpider(scrapy.Spider):
             span = spans_garbage[1:]  # [<span...>...</span>...</span>]
             garbage = spans_garbage[:1]  # [<style>...<span>...<div>]
 
-            item['ip'] = get_ip(garbage, span, div, clear_elements)
+            item['ip_address'] = get_ip(garbage, span, div, clear_elements)
             item['port'] = proxy.css('td')[2].xpath('text()').extract()[0].strip()
+
+            record_to_db = ParseProjectPipeline()
+            record_to_db.process_item(item, scrapy.Spider)
 
             yield item
 
